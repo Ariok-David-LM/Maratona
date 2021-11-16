e7a21b9b2c68ea90e74315397a0d6628e863d44c
Ariok-David-LM <ariokdavidl@gmail.com>
2021-11-15 20:53:52 -0600

    Desafio_1

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+      name='my_custom_sklearn_transforms',
+      version='1.0',
+      description='''
+            This is a sample python package for encapsulating custom
+            tranforms from scikit-learn into Watson Machine Learning
+      ''',
+      url='https://github.com/Ariok-David-LM/Maratona/',
+      author='León Moreno Ariok David',
+      author_email='ariokdavidl@yahoo.com',
+      license='',
+      packages=[
+            'my_custom_sklearn_transforms'
+      ],
+      zip_safe=False
+)
